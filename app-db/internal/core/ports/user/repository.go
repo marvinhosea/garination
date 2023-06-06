fbeb8467b14cb3f3b4f971d784f36dbd790f0854
oyamo <oyamo.xyz@gmail.com>
2023-06-06 14:21:04 +0300

    car proto

--- app-db/internal/core/ports/user/repository.go
+++ app-db/internal/core/ports/user/repository.go
@@ -9,4 +9,5 @@ type UserRepository interface {
 	GetUserMeta(ctx context.Context, userID string) (*model.UserMetum, error)
 	InsertUserMeta(ctx context.Context, arg model.UserMetum) (*model.UserMetum, error)
 	UpdateUserMeta(ctx context.Context, arg model.UserMetum) (*model.UserMetum, error)
+	UpdateUserDealership(ctx context.Context, userId, dealershipId string) (*model.UserMetum, error)
 }
