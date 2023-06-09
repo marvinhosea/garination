9e8b40079023ea1404c3f7eece2eb3cb344be8cf
oyamo <oyamo.xyz@gmail.com>
2023-06-09 12:39:49 +0300

    drlivery

--- app-api-gateway/internal/core/auth/ports/delivery.go
+++ app-api-gateway/internal/core/auth/ports/delivery.go
@@ -1,1 +1,12 @@
 package ports
+
+import (
+	"context"
+	"garination.com/gateway/internal/core/auth/dto"
+)
+
+type Delivery interface {
+	InitiateLogin(ctx context.Context, req *dto.AuthLoginRequest) (*dto.AuthLoginResponse, error)
+	InitiateRegister(ctx context.Context, req *dto.AuthRegisterRequest) (*dto.AuthRegisterResponse, error)
+	Logout(ctx context.Context, req *dto.AuthLogoutRequest) (*dto.AuthLogoutResponse, error)
+}
