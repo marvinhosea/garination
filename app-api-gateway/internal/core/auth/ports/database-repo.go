3c944419f42da30f6bd095f34fbdf404b039bbab
oyamo <oyamo.xyz@gmail.com>
2023-06-09 12:37:16 +0300

    database repo port

--- app-api-gateway/internal/core/auth/ports/database-repo.go
+++ app-api-gateway/internal/core/auth/ports/database-repo.go
@@ -1,1 +1,12 @@
 package ports
+
+import (
+	"context"
+	"garination.com/gateway/internal/core/auth/model"
+)
+
+type DatabaseRepo interface {
+	GetUserMeta(ctx context.Context, userID string) (*model.UserMetum, error)
+	InsertUserMeta(ctx context.Context, arg model.UserMetum) (*model.UserMetum, error)
+	UpdateUserMeta(ctx context.Context, arg model.UserMetum) (*model.UserMetum, error)
+}
