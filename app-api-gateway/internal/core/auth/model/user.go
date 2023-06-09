fc7b65a9ba3b8f570b780a2b4191c476b15974e0
oyamo <oyamo.xyz@gmail.com>
2023-06-09 12:41:33 +0300

    user model

---
+++ app-api-gateway/internal/core/auth/model/user.go
@@ -0,0 +1,12 @@
+package model
+
+type UserMetum struct {
+	UserMetaID   string
+	UserID       string
+	FacebookUrl  string
+	TwitterUrl   string
+	InstagramUrl string
+	LinkedinUrl  string
+	WebsiteUrl   string
+	DealershipID string
+}
