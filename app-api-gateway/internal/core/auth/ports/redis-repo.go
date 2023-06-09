8fd56361045330a1a23d3f8d55242cebaa40a672
oyamo <oyamo.xyz@gmail.com>
2023-06-09 12:41:51 +0300

    redis repo port

--- app-api-gateway/internal/core/auth/ports/redis-repo.go
+++ app-api-gateway/internal/core/auth/ports/redis-repo.go
@@ -1,1 +1,11 @@
 package ports
+
+import (
+	"context"
+	"garination.com/gateway/internal/core/auth/model"
+)
+
+type AuthRedisRepo interface {
+	GetUserMeta(ctx context.Context, userID string) (*model.UserMetum, error)
+	InsertUserMeta(ctx context.Context, arg model.UserMetum) (*model.UserMetum, error)
+}
