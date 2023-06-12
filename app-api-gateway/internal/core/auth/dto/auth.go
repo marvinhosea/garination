5acc48923e9111b0ad4106e5af5aa4e41646529d
oyamo <oyamo.xyz@gmail.com>
2023-06-12 11:44:01 +0300

    add validation

--- app-api-gateway/internal/core/auth/dto/auth.go
+++ app-api-gateway/internal/core/auth/dto/auth.go
@@ -1,10 +1,50 @@
 package dto
 
+import (
+	"errors"
+	"regexp"
+)
+
+const urlRegex = `^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$`
+
 // AuthLoginRequest ...
 type AuthLoginRequest struct {
 	RedirectURL string `json:"redirect_url"` // redirect_url is the url to redirect after login
 	Origin      string `json:"origin"`       // origin is the url of the client
 	Provider    string `json:"provider"`     // provider is the name of the provider
+}
+
+func (a AuthLoginRequest) Validate() error {
+	var issues error
+	if a.RedirectURL == "" {
+		issues = errors.Join(issues, errors.New("redirect_url is required"))
+	}
+
+	if a.Origin == "" {
+		issues = errors.Join(issues, errors.New("origin is required"))
+	}
+
+	if a.Provider == "" {
+		issues = errors.Join(issues, errors.New("provider is required"))
+	}
+
+	// validate provider (facebook, google, apple only)
+	if a.Provider != "facebook" && a.Provider != "google" && a.Provider != "apple" {
+		issues = errors.Join(issues, errors.New("provider is not supported"))
+	}
+
+	// validate redirect_url is a valid url
+	re := regexp.MustCompile(urlRegex)
+	if !re.MatchString(a.RedirectURL) {
+		issues = errors.Join(issues, errors.New("redirect_url is not a valid url"))
+	}
+
+	// validate origin is a valid url
+	if !re.MatchString(a.Origin) {
+		issues = errors.Join(issues, errors.New("origin is not a valid url"))
+	}
+
+	return issues
 }
 
 // AuthLoginResponse ...
@@ -20,6 +60,39 @@ type AuthRegisterRequest struct {
 	Provider    string `json:"provider"`     // provider is the name of the provider
 }
 
+func (a AuthRegisterRequest) Validate() error {
+	var issues error
+	if a.RedirectURL == "" {
+		issues = errors.Join(issues, errors.New("redirect_url is required"))
+	}
+
+	if a.Origin == "" {
+		issues = errors.Join(issues, errors.New("origin is required"))
+	}
+
+	if a.Provider == "" {
+		issues = errors.Join(issues, errors.New("provider is required"))
+	}
+
+	// validate provider (facebook, google, apple only)
+	if a.Provider != "facebook" && a.Provider != "google" && a.Provider != "apple" {
+		issues = errors.Join(issues, errors.New("provider is not supported"))
+	}
+
+	// validate redirect_url is a valid url
+	re := regexp.MustCompile(urlRegex)
+	if !re.MatchString(a.RedirectURL) {
+		issues = errors.Join(issues, errors.New("redirect_url is not a valid url"))
+	}
+
+	// validate origin is a valid url
+	if !re.MatchString(a.Origin) {
+		issues = errors.Join(issues, errors.New("origin is not a valid url"))
+	}
+
+	return issues
+}
+
 // AuthRegisterResponse ...
 type AuthRegisterResponse struct {
 	RedirectURL  string `json:"redirect_url"`   // redirect_url is the url to redirect after login
@@ -28,6 +101,10 @@ type AuthRegisterResponse struct {
 
 // AuthLogoutRequest ...
 type AuthLogoutRequest struct {
+}
+
+func (r AuthLogoutRequest) Validate() error {
+	return nil
 }
 
 // AuthLogoutResponse ...
