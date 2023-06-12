dcbf3136ea4b29514c10b89acc22a287d6afa5ed
oyamo <oyamo.xyz@gmail.com>
2023-06-12 14:18:13 +0300

    insert dealership logic

--- app-db/internal/core/adapters/service/dealership.go
+++ app-db/internal/core/adapters/service/dealership.go
@@ -3,11 +3,13 @@ package service
 import (
 	"context"
 	"garination.com/db/internal/core/ports/dealership"
+	"garination.com/db/internal/core/ports/user"
 	"garination.com/db/internal/platform/postgres"
 )
 
 type dealershipService struct {
 	dealershipRepo dealership.DealershipRepository
+	userRepo       user.UserRepository
 }
 
 func (d dealershipService) GetUserDealership(ctx context.Context, userID string) (postgres.Dealership, error) {
@@ -15,7 +17,36 @@ func (d dealershipService) GetUserDealership(ctx context.Context, userID string)
 }
 
 func (d dealershipService) InsertDealership(ctx context.Context, arg postgres.InsertDealershipParams) (postgres.Dealership, error) {
-	return d.dealershipRepo.InsertDealership(ctx, arg)
+	dealership, err := d.dealershipRepo.InsertDealership(ctx, arg)
+	if err != nil {
+		return postgres.Dealership{}, err
+	}
+
+	// update user meta
+	userMeta, err := d.userRepo.GetUserMeta(ctx, arg.OwnerID)
+	if err != nil {
+		// rollback dealership
+		err = d.dealershipRepo.DeleteDealership(ctx, dealership.DealershipID)
+		if err != nil {
+			return postgres.Dealership{}, err
+		}
+		return postgres.Dealership{}, err
+	}
+
+	userMeta.DealershipID.String = dealership.DealershipID
+	userMeta.DealershipID.Valid = true
+
+	_, err = d.userRepo.UpdateUserDealership(ctx, userMeta.UserID, dealership.DealershipID)
+	if err != nil {
+		// rollback dealership
+		err = d.dealershipRepo.DeleteDealership(ctx, dealership.DealershipID)
+		if err != nil {
+			return postgres.Dealership{}, err
+		}
+		return postgres.Dealership{}, err
+	}
+
+	return dealership, nil
 }
 
 func (d dealershipService) UpdateDealership(ctx context.Context, arg postgres.UpdateDealershipParams) (postgres.Dealership, error) {
@@ -30,6 +61,6 @@ func (d dealershipService) DeleteDealership(ctx context.Context, id string) erro
 	return d.dealershipRepo.DeleteDealership(ctx, id)
 }
 
-func NewDealershipService(dealershipRepo dealership.DealershipRepository) dealership.DealershipService {
-	return &dealershipService{dealershipRepo: dealershipRepo}
+func NewDealershipService(dealershipRepo dealership.DealershipRepository, userRepo user.UserRepository) dealership.DealershipService {
+	return &dealershipService{dealershipRepo: dealershipRepo, userRepo: userRepo}
 }
