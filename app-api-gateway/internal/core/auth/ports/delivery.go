d6f996b96c9b75327cacf9ca10904dec04411ffd
oyamo <oyamo.xyz@gmail.com>
2023-06-13 14:40:21 +0300

    http delivery for dealership

--- app-api-gateway/internal/core/auth/ports/delivery.go
+++ app-api-gateway/internal/core/auth/ports/delivery.go
@@ -6,6 +6,10 @@ import (
 
 type AuthHttpHandler interface {
 	InitiateLogin() gin.HandlerFunc
+	LoginCallback() gin.HandlerFunc
 	InitiateRegister() gin.HandlerFunc
+	RegisterCallback() gin.HandlerFunc
+	UpdateUserMeta() gin.HandlerFunc
+	GetUserMeta() gin.HandlerFunc
 	Logout() gin.HandlerFunc
 }
--- app-api-gateway/internal/core/dealership/ports/delivery.go
+++ app-api-gateway/internal/core/dealership/ports/delivery.go
@@ -1,1 +1,11 @@
 package ports
+
+import "github.com/gin-gonic/gin"
+
+type DealerShiphandler interface {
+	GetDealershipByID() gin.HandlerFunc
+	GetDealershipByUserID() gin.HandlerFunc
+	UpdateDealership() gin.HandlerFunc
+	CreateDealership() gin.HandlerFunc
+	DeleteDealership() gin.HandlerFunc
+}
